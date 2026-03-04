a63e00bf938478254c806c4f42126eb2c246781b
Amit paudel <amitpaudel2023@gmail.com>
2026-03-04 15:06:42 +0545

    new file added

--- tests/ui/uploadFile.spec.ts
+++ tests/ui/uploadFile.spec.ts
@@ -1,13 +1,10 @@
 import { test, expect } from "@playwright/test";
-import path from "node:path";
 
 test("upload file test", async ({ page }) => {
   await page.goto("https://demoqa.com/upload-download", {
     waitUntil: "domcontentloaded",
   });
-  await page
-    .locator("#uploadFile")
-    .setInputFiles("C:/Users/AMIT/Desktop/amit.png");
+  await page.locator("#uploadFile").setInputFiles("tests/ui/assets/amit.png");
 
   await expect(page.locator("#uploadedFilePath")).toContainText(/amit.png/i);
 });
