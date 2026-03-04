1a30bfee2ea2b63724697574719f5c13394a7f6d
Amit paudel <amitpaudel2023@gmail.com>
2026-03-04 14:51:45 +0545

    updated sauce login

--- tests/ui/sauceLogin.spec.ts
+++ tests/ui/sauceLogin.spec.ts
@@ -1,6 +1,6 @@
 import { uiFixture } from "../fixtures/uiFixture/auth.spec";
 import { expect } from "@playwright/test";
 
-uiFixture("Add to cart", async ({ login }) => {
+uiFixture("Add to cart updated", async ({ login }) => {
   await expect(login).toHaveURL("https://www.saucedemo.com/inventory.html");
 });
